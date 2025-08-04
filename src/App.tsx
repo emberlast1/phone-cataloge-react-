c3f492321051eaf98d8bd99de8b8fbce1243923a
Ostap Hedz <156020698+emberlast1@users.noreply.github.com>
2025-08-04 18:47:46 +0200

    final version

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import React from 'react';
 import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { Footer } from './components/Footer';
+import { Header } from './components/Header/Header';
+import { Outlet } from 'react-router-dom';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="App">
+      <Header />
+      <div className="main">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
